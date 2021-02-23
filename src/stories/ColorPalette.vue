d0794ad9b6b5644b8d00cfaf23564df264ca889d
Mike McDonald <michael.mcdonald@portlandoregon.gov>
2021-02-23 07:10:18 +0000

    Add color palette stories Used to show of colors in the tailwind theme

---
+++ src/stories/ColorPalette.vue
@@ -0,0 +1,47 @@
+<template>
+  <ul class="flex -mx-3">
+    <li
+      v-for="(weight, index) in weights"
+      :key="index"
+      class="flex flex-col my-1 mx-3"
+    >
+      <span
+        class="w-16 h-12 rounded-lg"
+        :class="[`bg-${color}-${weight}`]"
+      ></span>
+      <div class="flex flex-col">
+        <span>{{ weight }}</span>
+        <span class="text-base text-gray-500">{{
+          colorValue(color, weight)
+        }}</span>
+      </div>
+    </li>
+  </ul>
+</template>
+
+<script>
+import resolveConfig from 'tailwindcss/resolveConfig';
+import tailwindConfig from '../../tailwind.config.js';
+
+const fullConfig = resolveConfig(tailwindConfig);
+
+export default {
+  name: 'ColorPalette',
+  props: {
+    color: {
+      type: String,
+      required: true
+    }
+  },
+  data() {
+    return {
+      weights: [100, 200, 300, 400, 500, 600, 700, 800, 900]
+    };
+  },
+  methods: {
+    colorValue(color, weight) {
+      return fullConfig.theme.colors[color][weight];
+    }
+  }
+};
+</script>
