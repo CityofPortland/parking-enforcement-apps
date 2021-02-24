41252b6faa51e099b9d6da4391af9bf5c472f989
Mike McDonald <michael.mcdonald@portlandoregon.gov>
2021-02-24 11:40:35 -0800

    PEA-25 Add story ordering

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,10 @@
 import '../src/index.css'
 
 export const parameters = {
+  options: {
+    storySort: {
+      order: ['Design', 'Element', 'Component']
+    },
+  },
   actions: { argTypesRegex: "^on[A-Z].*" },
 }
