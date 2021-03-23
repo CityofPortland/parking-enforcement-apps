f516817b2f144c6d70ba57db0d6ad50354280dc6
Mike McDonald <michael.mcdonald@portlandoregon.gov>
2021-03-23 20:32:31 +0000

    PEA-2 Refactor App view structure

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,62 @@
 <template>
-  <Header color="white" open="open">
+  <Header
+    color="white"
+    :open="menuOpen"
+    @toggle="menuOpen = !menuOpen"
+    :lang="locale"
+  >
     <template v-slot:branding>
-      <Logo class="w-16 mr-3 hidden md:inline-flex" />
-      <h1 class="truncate">Parking Enforcement - Area Permit Lookup</h1>
-      <select
+      <a href="/" class="w-full flex items-center">
+        <Logo class="w-16 mr-3 flex-shrink-0" />
+        <span class="truncate text-xl">Parking Enforcement</span>
+      </a>
+    </template>
+
+    <template v-slot:menu>
+      <div class="p-4 md:ml-auto">
+        <select
           v-model="locale"
-          class="h-10 pl-3 pr-6 text-base placeholder-gray-600 border
-          rounded-md float-right"
+          class="px-3 py-2 placeholder-gray-600 rounded-md text-gray-900 border border-gray-500 shadow-md bg-gray-100"
         >
           <option value="en">English</option>
           <option value="no">Norwegian</option>
         </select>
+      </div>
     </template>
   </Header>
-
-  <div class="p-5">
-    <PermitLookup />
-  </div>
+  <main class="max-w-7xl mx-auto px-4 mt-8" :lang="locale">
+    <h1 class="text-2xl font-bold mb-8">Area Parking Permit Lookup</h1>
+    <section class="max-w-xl">
+      <p>
+        Use the form below to enter a vehicle's license plate number, and select
+        a parking permit zone, then query whether the vehicle has an active
+        parking permit for the selected zone. For more information
+        <a
+          href="https://www.portland.gov/transportation/parking/appp-info"
+          class="border-b-2 border-current font-semibold"
+          >see here</a
+        >.
+      </p>
+      <PermitLookup class="mt-8" />
+    </section>
+  </main>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import Header from './components/header/Header.vue';
-import PermitLookup from './components/PermitLookup.vue';
-import Logo from './assets/Logo.vue';
+import { defineComponent, ref } from 'vue';
 import { useI18n } from 'vue-i18n';
+
+import Header from '@/components/header/Header.vue';
+import Logo from '@/assets/Logo.vue';
+import PermitLookup from '@/components/PermitLookup.vue';
+
 export default defineComponent({
   setup() {
     const { t, locale } = useI18n();
-    return { t, locale };
+
+    const menuOpen = ref(false);
+
+    return { t, locale, menuOpen };
   },
   components: {
     Header,
