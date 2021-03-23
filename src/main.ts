cf93c26d1a420bf52a098558c8fe4f6a4f4017ed
Mike McDonald <michael.mcdonald@portlandoregon.gov>
2021-03-23 18:31:18 +0000

    PEA-2 Set default lang to english

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,17 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import "./index.css";
-import "./registerServiceWorker";
-import store from "./store";
-import { createI18n } from 'vue-i18n'
+import { createApp } from 'vue';
+import App from './App.vue';
+import './index.css';
+import './registerServiceWorker';
+import store from './store';
+import { createI18n } from 'vue-i18n';
 
 const i18n = createI18n({
-  locale: 'no', // set locale
+  locale: 'en', // set locale
   fallbackLocale: 'en', // set fallback locale
   legacy: false
-})
-
+});
 
 createApp(App)
   .use(i18n)
   .use(store)
-  .mount("#app");
-
-
-  
+  .mount('#app');
