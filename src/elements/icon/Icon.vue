6264aa5f557e0dea74fb14ca4f255c7bc6d28a3d
Mike McDonald <michael.mcdonald@portlandoregon.gov>
2021-03-26 19:40:03 -0700

    PEA-2 Add generic icon component

---
+++ src/elements/icon/Icon.vue
@@ -0,0 +1,36 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke="currentColor"
+  >
+    <path
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="2"
+      :d="path"
+    />
+  </svg>
+</template>
+
+<script lang="ts">
+import { defineComponent, computed } from 'vue';
+
+import icons from './icons';
+
+export default defineComponent({
+  name: 'Icon',
+  props: {
+    type: {
+      type: String,
+      required: true
+    }
+  },
+  setup(props) {
+    return {
+      path: computed(() => icons[props.type])
+    };
+  }
+});
+</script>
---
+++ src/elements/icon/icons.ts
@@ -0,0 +1,5 @@
+export default {
+  check: 'M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z',
+  'x-circle':
+    'M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z'
+} as { [key: string]: string };
