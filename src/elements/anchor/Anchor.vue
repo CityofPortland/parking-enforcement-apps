adc94c67ef2ec27cacf847eaa2b94a87a25d6c98
Mike McDonald <michael.mcdonald@portlandoregon.gov>
2021-04-02 00:42:50 +0000

    PEA-26 Add Anchor element

---
+++ src/elements/anchor/Anchor.vue
@@ -0,0 +1,42 @@
+<template>
+  <a
+    :href="url"
+    :class="{
+      'inline-flex': external,
+      'items-center': external
+    }"
+  >
+    <span
+      class="inline-flex items-center border-b-2 border-current font-medium hover:font-bold"
+    >
+      <slot>{{ url }}</slot>
+    </span>
+    <Icon v-if="external" type="external-link" class="h-4 w-4 ml-1" />
+  </a>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+import Icon from '@/elements/icon/Icon.vue';
+
+export default defineComponent({
+  name: 'Anchor',
+  components: { Icon },
+  props: {
+    url: {
+      type: String,
+      required: true
+    },
+    border: {
+      type: Boolean,
+      default: true
+    }
+  },
+  setup(props) {
+    return {
+      external: props.url.startsWith('http')
+    };
+  }
+});
+</script>
