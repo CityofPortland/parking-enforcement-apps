ef9dc037d9b14ccb516b2a4805ef183dd884c5ca
Mike McDonald <michael.mcdonald@portlandoregon.gov>
2021-04-02 16:36:02 -0700

    PEA-38 Add disclaimer page

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
 
+import Disclaimer from '@/pages/Disclaimer.vue';
 import PermitLookup from '@/pages/PermitLookup.vue';
 
 const routes: RouteRecordRaw[] = [
   { path: '/', redirect: '/permit-lookup' },
-  { path: '/permit-lookup', component: PermitLookup }
+  { path: '/permit-lookup', component: PermitLookup },
+  { path: '/disclaimer', component: Disclaimer }
 ];
 
 export default createRouter({
