d65a087f821b308ca86790525fb932020d50f172
Mike McDonald <michael.mcdonald@portlandoregon.gov>
2021-04-13 01:12:23 +0000

    PEA-40 Configure Vue CLI for production sub path

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,5 @@
-const path = require('path');
-
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production' ? '/parking/' : '',
   configureWebpack: {
     module: {
       rules: [
@@ -19,33 +18,5 @@ module.exports = {
       .use('i18n')
       .loader('@intlify/vue-i18n-loader')
       .end();
-  },
-  /*chainWebpack: config => {
-    config.module
-      //.rule('i18n-resource')
-      .rule('i18n')
-      .resourceQuery(/blockType=i18n/)
-      .test(/\.(json5?|ya?ml)$/)
-      .include.add(path.resolve(__dirname, './src/locales'))
-      .end()
-      .type('javascript/auto')
-      .use('i18n-resource')
-      .loader('@intlify/vue-i18n-loader');
-    config.module
-      .rule('i18n')
-      .resourceQuery(/blockType=i18n/)
-      .type('javascript/auto')
-      .use('i18n')
-      .loader('@intlify/vue-i18n-loader')
-      .end(); // cory added
-  },*/
-/*
-  pluginOptions: {
-    i18n: {
-      locale: 'en',
-      fallbackLocale: 'no',
-      localeDir: 'assets/locales',
-      enableInSFC: true
-    }
-  }*/
+  }
 };
