ed2f9e3d3f29c7baf3ea0fa62d706eb3fb46782c
Mike McDonald <michael.mcdonald@portlandoregon.gov>
2021-05-12 15:50:29 -0700

    PEA-43 Refactor result for displaying limit list Refactor layout and content language

--- src/components/permit/PermitLookupResult.vue
+++ src/components/permit/PermitLookupResult.vue
@@ -2,53 +2,50 @@
   <Box
     :color="permit.isValid ? 'green' : 'red'"
     variant="light"
-    class="p-6 flex flex-col items-center justify-center space-y-4 border border-current rounded-md shadow-md"
+    class="flex flex-col md:flex-row p-6 border border-current rounded-md shadow-md"
   >
-    <Icon
-      :type="permit.isValid ? 'check-circle' : 'x-circle'"
-      class="h-12 w-12"
-    />
+    <Box class="flex-shrink-0 self-center md:self-start md:mt-1 md:mr-4">
+      <Icon
+        :type="permit.isValid ? 'check-circle' : 'x-circle'"
+        class="h-10 w-10"
+      />
+    </Box>
 
-    <span class="font-semibold block text-2xl text-center">
-      <!-- Permit Found for '{{ plateSearchedFor }}'! -->
-      {{ summary }}
-    </span>
+    <Box class="flex flex-col space-y-3">
+      <i18n-t
+        :keypath="permit.isValid ? 'permitFoundHeader' : 'permitNotFoundHeader'"
+        tag="p"
+        class="font-semibold text-2xl leading-snug"
+      >
+        <template v-slot:licensePlate>{{ permit.licensePlate }}</template>
+      </i18n-t>
 
-    <p class="block text-center">
-      {{ body }}
-    </p>
+      <i18n-t
+        :keypath="permit.isValid ? 'permitFoundBody' : 'permitNotFoundBody'"
+        tag="p"
+      >
+        <template v-slot:licensePlate>{{ permit.licensePlate }}</template>
+        <template v-slot:zone>Zone {{ permit.zone.id }}</template>
+      </i18n-t>
 
-    <i18n-t
-      v-if="!permit.isValid"
-      keypath="permitNotFoundEnforcementInfo"
-      tag="p"
-      class="block text-center"
-    >
-      <template v-slot:zoneName>'Zone {{ permit.zone.id }}'</template>
-    </i18n-t>
+      <ul v-if="!permit.isValid" class="pl-4 list-disc list-outside">
+        <li v-for="(info, index) in permit.zone.enforcementInfo" :key="index">
+          {{ capitalize(info.visitorLimit) }} during the hours of
+          {{ info.enforcementHours }}
+        </li>
+      </ul>
 
-    <ul class="list-disc">
-      <li v-for="(info, index) in permit.zone.enforcementInfo" :key="index">
-        {{ capitalize(info.visitorLimit) }} during the hours of
-        {{ info.enforcementHours }}
-      </li>
-    </ul>
-
-    <i18n-t
-      v-if="!permit.isValid"
-      keypath="permitNotFoundCaveat"
-      tag="p"
-      class="block text-center"
-    >
-      <template v-slot:callEnforcementLink>
-        <Anchor url="tel:503-823-5195">503-823-5195</Anchor>
-      </template>
-    </i18n-t>
+      <i18n-t v-if="!permit.isValid" keypath="permitNotFoundCaveat" tag="p">
+        <template v-slot:callEnforcementLink>
+          <Anchor url="tel:503-823-5195">503-823-5195</Anchor>
+        </template>
+      </i18n-t>
+    </Box>
   </Box>
 </template>
 
 <script lang="ts">
-import { defineComponent, computed } from 'vue';
+import { defineComponent } from 'vue';
 import { useI18n } from 'vue-i18n';
 
 import Anchor from '@/elements/anchor/Anchor.vue';
@@ -73,24 +70,6 @@ export default defineComponent({
       t,
       licensePlate: props.permit.licensePlate,
       zone: props.permit.zone,
-      summary: computed(() =>
-        props.permit.isValid
-          ? t('permitFoundHeader', { licensePlate: props.permit.licensePlate })
-          : t('permitNotFoundHeader', {
-              licensePlate: props.permit.licensePlate
-            })
-      ),
-      body: computed(() =>
-        props.permit.isValid
-          ? t('permitFoundBody', {
-              licensePlate: props.permit.licensePlate,
-              zone: `Zone ${props.permit.zone.id}`
-            })
-          : t('permitNotFoundBody', {
-              licensePlate: props.permit.licensePlate,
-              zone: `Zone ${props.permit.zone.id}`
-            })
-      ),
       capitalize: (str: string) => {
         return str.charAt(0).toUpperCase() + str.slice(1);
       }
@@ -105,11 +84,10 @@ export default defineComponent({
     "permitFoundHeader": "Active area parking permit found for '{licensePlate}'!",
     "permitNotFoundHeader": "No active area parking permit found for '{licensePlate}'.",
     "permitFoundBody": "Vehicle with license plate '{licensePlate}' has an active area parking permit in '{zone}'.",
-    "permitNotFoundBody": "Vehicle with license plate '{licensePlate}' does not have an active area parking permit in '{zone}'.",
-    "permitNotFoundEnforcementInfo" : "Visitor parking in {zoneName} has the following limits:",
+    "permitNotFoundBody": "Vehicle with license plate '{licensePlate}' does not have an active area parking permit in '{zone}'. A vehicle without an active area parking permit in '{zone}' can park up to the following time limits:",
     "permitNotFoundMap": "{useMapLink} to view visitor time limits and enforcement hours in your zone.",
     "useMap": "Use the zone enforcement map",
-    "permitNotFoundCaveat": "A vehicle without an active area parking permit can park up to the visitor time limit in the zone. Vehicles might have other permits allowing them to park in this area, not all vehicles with a result ‘no active area parking permit found’ will be in violation. If you’d like to report this vehicle to Parking Enforcement call {callEnforcementLink}, choose option 1 to report a vehicle."
+    "permitNotFoundCaveat": "Vehicles might have other permits allowing them to park in this area, not all vehicles with a result ‘no active area parking permit found’ will be in violation. If you’d like to report this vehicle to Parking Enforcement call {callEnforcementLink}, choose option 1 to report a vehicle."
   },
   "no": {
     "permitFoundHeader": "Tillatelse funnet for '{licensePlate}'!",
