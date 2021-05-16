b57c23a4778bc0d777e5fd7aca3da3c1d1d1520a
Mike McDonald <michael.mcdonald@portlandoregon.gov>
2021-05-16 09:33:04 -0700

    PEA-16 Refactor icon stories to help re-use Split between TS and MDX files

--- src/elements/icon/Icon.stories.ts
+++ src/elements/icon/Icon.stories.ts
@@ -1,20 +1,16 @@
-import { Meta, Story } from '@storybook/vue3';
+import { Story } from '@storybook/vue3';
 
 import Component from './Icon.vue';
 import icons from './icons';
 
-export default {
-  title: 'Element/Icon',
-  component: Component,
-  argTypes: {
-    type: {
-      control: {
-        type: 'select',
-        options: Object.keys(icons)
-      }
+export const IconArgs = {
+  type: {
+    control: {
+      type: 'select',
+      options: Object.keys(icons)
     }
   }
-} as Meta;
+};
 
 const Template: Story = (args, { argTypes }) => ({
   props: Object.keys(argTypes),
@@ -22,7 +18,7 @@ const Template: Story = (args, { argTypes }) => ({
   template: `<Component v-bind="$props" class="h-12 w-12" />`
 });
 
-export const Icon = Template.bind({});
-Icon.args = {
+export const Basic = Template.bind({});
+Basic.args = {
   type: 'check-circle'
 };
